0867d4ea6ce839017498a9d3bda9b46ec845ae0e
LT <1425.lt@gmail.com>
2024-03-08 02:14:07 +0700

    init

--- pages/dashboard.py
+++ pages/dashboard.py
@@ -146,7 +146,7 @@ for col_index, col in enumerate([col3, col4, col5, col6, col7], start=3):
         if data is not None:
             binary_columns = []
             for col_name in data.columns:
-                if data[col_name].isin([0, 1]).all():  # Check if all values are 0 or 1
+                if data[col_name].isin([0, 1]).all():  
                     binary_columns.append(col_name)
             string_columns = data.select_dtypes(include=['object']).columns
             if not binary_columns and string_columns.empty:
@@ -163,13 +163,13 @@ for col_index, col in enumerate([col3, col4, col5, col6, col7], start=3):
                         pie_data = data[selected_column].value_counts()
                         fig_pie = px.pie(names=pie_data.index, values=pie_data.values, title=selected_column,
                                          color=pie_data.index, color_discrete_map={True: 'blue', False: 'green'})
-                        fig_pie.update_layout(autosize=True, width=300, height=300)  # Adjust width and height as desired
+                        fig_pie.update_layout(autosize=True, width=300, height=300) 
                         fig_pie.update_traces(textposition='inside', textinfo='percent+label')  
                         st.plotly_chart(fig_pie)
                     elif selected_column in string_columns:
                         pie_data = data[selected_column].value_counts()
                         fig_pie = px.pie(names=pie_data.index, values=pie_data.values, title=selected_column)
-                        fig_pie.update_layout(autosize=True, width=300, height=300)  # Adjust width and height as desired
+                        fig_pie.update_layout(autosize=True, width=300, height=300)  
                         fig_pie.update_traces(textposition='inside', textinfo='percent+label')  
                         st.plotly_chart(fig_pie)
                     else:
